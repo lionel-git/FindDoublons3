b1e882093e00eac59e8127a524f83c2a5a8d3d48
Lionel Desorme <lionel.desorme@free.fr>
2019-03-09 17:36:55 +0100

    Initial Commit

---
+++ FindDoublons3/FD3_UI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindDoublons3
+{
+    public partial class FD3_UI : Form
+    {
+        public FD3_UI()
+        {
+            InitializeComponent();
+        }
+    }
+}
